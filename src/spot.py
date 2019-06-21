609996336301ec5db2cd23864145653b2af3242f
Samuel Whittenberger <sbw98978@gmail.com>
2019-06-21 19:29:34 -0400

    small clarification

--- src/spot.py
+++ src/spot.py
@@ -5,8 +5,8 @@ username = input('enter spotify username: ')
 
 #after inputing username, you will be redirected to a url, copy that url and paste into console to authenticate
 
-#examples of retrieving graphed info below
-#i recommend using only one object at a time, but can do both, will have to authenticate multiple times
+#examples of retrieving graphed info below, uncomment one object and run this file to view results
+#i recommend using only one object at a time, but can do both, will have to authenticate multiple times if you do
 
 #tracks = track.tracks(username)
 #tracks.show_top_artists_graph(10)
