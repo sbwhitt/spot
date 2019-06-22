9aa1a7f20ae4b268575536c89265afa424dbd2d7
Samuel Whittenberger <sbw98978@gmail.com>
2019-06-21 21:47:50 -0400

    add retrieval of individual playlist pop avg

--- src/playlists.py
+++ src/playlists.py
@@ -3,22 +3,24 @@ import spotipy.util as util
 from config import *
 import matplotlib
 import matplotlib.pyplot as plt
+import math
 
 class playlists:
     def __init__(self, username):
         scope = 'playlist-read-private'
         token = util.prompt_for_user_token(username, scope, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, 
             redirect_uri=SPOTIPY_REDIRECT_URI)
-        spot = spotipy.Spotify(auth=token)
+        self.spot = spotipy.Spotify(auth=token)
 
         self.user_playlists = []
         self.username = username
-        self.user_playlists_raw = spot.user_playlists(self.username, limit=50, offset=0)
+        self.user_playlists_raw = self.spot.user_playlists(self.username, limit=50, offset=0)
 
         self.__parse_user_playlists()
     
     def __parse_user_playlists(self):
         for i, playlist in enumerate(self.user_playlists_raw['items']):
+            #print(playlist)
             if playlist['owner']['display_name'] == self.username:
                 self.user_playlists.append([str(playlist['name']), playlist['tracks']['total']])
 
@@ -36,3 +38,24 @@ class playlists:
         fig, chart = plt.subplots()
         chart.bar(playlistNames, playlistSongs)
         plt.show()
+
+    def print_playlist_popularity_average(self, name):
+        target_playlist = ""
+
+        for i, playlist in enumerate(self.user_playlists_raw['items']):
+            if playlist['name'] == name:
+                target_playlist = playlist
+                break
+        
+        if target_playlist == "":
+            print("invalid playlist name")
+            return
+        
+        num_tracks = 0
+        total_pop = 0
+
+        tracks = self.spot.user_playlist(self.username, target_playlist['id'], fields="tracks,next")['tracks']['items']
+        for track in tracks:
+            total_pop += track['track']['popularity']
+        
+        print("playlist " + name + " popularity average: " + str(math.floor(total_pop/num_tracks)))
--- src/spot.py
+++ src/spot.py
@@ -1,15 +1,17 @@
 import playlists as play
 import tracks as track
 
-username = input('enter spotify username: ')
+username = input("enter spotify username: ")
 
+#you will have to register a spotify app to get the proper credentials to make authenticated calls
 #after inputing username, you will be redirected to a url, copy that url and paste into console to authenticate
 
-#examples of retrieving graphed info below, uncomment one object and run this file to view results
+#examples of retrieving graphed/printed info below, uncomment one object block and run this file to view results
 #i recommend using only one object at a time, but can do both, will have to authenticate multiple times if you do
 
 #tracks = track.tracks(username)
 #tracks.show_top_artists_graph(10)
 
 #p = play.playlists(username)
+#p.print_playlist_popularity_average("sr")
 #p.show_graph()
